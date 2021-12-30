c4f5ec03757c331e5cd8d7a13c0b6be1989feca6
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-12-30 20:54:40 +0530

    my-wallet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import React from "react"
+
 
 function App() {
+
+  const [address, setAddress] = React.useState("");
+  const [ balance, setBalance ] = React.useState("");
+  const [query, setQuery] = React.useState("");
+
+  const handleWalletBalance = async ()=> {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    let balance = await provider.getBalance(query)
+    setBalance("Your Balance : "+ethers.utils.formatEther(balance));
+  }
+
+  const handleConnectWallet = async ()=> {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setAddress("Your Address : "+await signer.getAddress());
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <input type="text" placeholder="enter your address" style={{padding:"10px"}} value={query} onChange={(e)=>setQuery(e.target.value)}/>
+       <p>{balance}</p>
+       <button style={{margin:"10px"}} onClick={handleWalletBalance}>Get Balance</button>
+       <button style={{margin:"10px"}} onClick={handleConnectWallet}>Connect Wallet</button>
+        <p>{address}</p>
       </header>
     </div>
   );
